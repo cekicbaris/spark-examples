e36f28bbaba811b4990c88acd5c817914a1fc1ed
Baris Cekic <baris@diskover.net>
2022-06-20 17:04:54 +0100

    Update delta.py

--- delta.py
+++ delta.py
@@ -1,18 +1,11 @@
 import pyspark
 from delta import *
+from pyspark.sql import SparkSession
 
-builder = (
-    pyspark.sql.SparkSession.builder.appName("MyApp")
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config(
-        "spark.sql.catalog.spark_catalog",
-        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
-    )
-)
 
-spark = configure_spark_with_delta_pip(builder).getOrCreate()
-print(spark)
+spark =  SparkSession.getOrCreate()
 
 url = "s3a://co-datazone-public/SalesRecords/SalesRecords.csv"
 
 df = spark.read.format("csv").option("header", "true").option("sep", ",").load(url)
+df.show()
