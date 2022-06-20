e43c8de9b07540c4150171821f75a49c55d6a4f3
Baris Cekic <baris@diskover.net>
2022-06-20 17:08:23 +0100

    Create delta_test.py

---
+++ delta_test.py
@@ -0,0 +1,9 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark =  SparkSession.getOrCreate()
+
+url = "s3a://co-datazone-public/SalesRecords/SalesRecords.csv"
+
+df = spark.read.format("csv").option("header", "true").option("sep", ",").load(url)
+df.show()
