4a89f3eac82bfea622d11b1029b7cbb610106915
Baris Cekic <baris@diskover.net>
2022-06-20 17:09:46 +0100

    Update and rename delta_test.py to delta_test2.py

--- delta_test2.py
+++ delta_test2.py
@@ -1,7 +1,9 @@
 import pyspark
 from pyspark.sql import SparkSession
 
-spark =  SparkSession.getOrCreate()
+spark =  ( SparkSession.builder
+         .getOrCreate()  
+        )
 
 url = "s3a://co-datazone-public/SalesRecords/SalesRecords.csv"
 
