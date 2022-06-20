af0e8316f879da1c7444834cb898a11776ab9f57
Baris Cekic <baris@diskover.net>
2022-06-20 19:17:54 +0100

    Update and rename delta_test7.py to delta_test8.py

--- delta_test8.py
+++ delta_test8.py
@@ -2,9 +2,11 @@ import pyspark
 from pyspark.sql import SparkSession
 from delta import *
 
-spark =  ( SparkSession.builder
-         .getOrCreate()  
-        )
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 url = "s3a://co-datazone-public/SalesRecords/1000 Sales Records.csv"
 
